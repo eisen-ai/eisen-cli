daab7a52ca247eb4b59dd6f497d67657dab349b1
Eisen AI <none@nobody.com>
2020-05-23 19:14:02 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
@@ -12,6 +12,7 @@ setup(
     description='Eisen Command Line Interface (CLI)',
     packages=find_packages(),
     install_requires=install_requires,
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'eisen = eisen_cli.main:cli'
